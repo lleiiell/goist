7e5a47951ff3856806ceb177cfbf8f9e5f0ebb5c
lleiiell <lleiiell@live.com>
2023-08-25 14:50:39 +0800

    add str func: IsStrEmpty

--- string.go
+++ string.go
@@ -1,13 +1,18 @@
 package goist
 
 import (
-    "crypto/md5"
-    "encoding/hex"
+	"crypto/md5"
+	"encoding/hex"
+	"strings"
 )
 
-func MD5(str string) string {
-    c := md5.New()
-    c.Write([]byte(str))
+func IsStrEmpty(str string) bool {
+	return strings.TrimSpace(str) == ""
+}
 
-    return hex.EncodeToString(c.Sum(nil))
+func MD5(str string) string {
+	c := md5.New()
+	c.Write([]byte(str))
+
+	return hex.EncodeToString(c.Sum(nil))
 }
--- string_test.go
+++ string_test.go
@@ -1,16 +1,20 @@
-package goist
+package goist_test
 
 import (
-    "fmt"
-    "golang.org/x/example/stringutil"
+	"fmt"
+	"github.com/lleiiell/goist"
 )
 
-func ExampleMD5() {
-    fmt.Println(MD5("hello"))
-    // Output: 5d41402abc4b2a76b9719d911017c592
+func ExampleStrIsEmpty() {
+	s1 := ""
+	s2 := "a bc"
+	s3 := "  "
+	s4 := "\n \t"
+	fmt.Println(goist.IsStrEmpty(s1), goist.IsStrEmpty(s2), goist.IsStrEmpty(s3), goist.IsStrEmpty(s4))
+	// Output: true false true true
 }
 
-func ExampleReverse() {
-    fmt.Println(stringutil.Reverse("hello"))
-    // Output: olleh
+func ExampleMD5() {
+	fmt.Println(goist.MD5("hello"))
+	// Output: 5d41402abc4b2a76b9719d911017c592
 }
