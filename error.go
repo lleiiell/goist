9808496231df9af515d79e5d4079c89a1627dc42
lleiiell <lleiiell@live.com>
2023-10-24 16:21:18 +0800

    error: add ErrCodexxx

--- error.go
+++ error.go
@@ -2,11 +2,27 @@ package goist
 
 import "fmt"
 
+// code err
+
+const ErrCodeCommon = 1001
+const ErrCodeParam = 1002
+const ErrCodeAuth = 1003
+
+// component err
+
+const ErrCodeDb = 2001
+const ErrCodeRedis = 2002
+const ErrCodeMySQL = 2003
+
+// external err
+
+const ErrCodeApi = 3001
+
 type Err struct {
-    ErrCode interface{}
-    ErrMsg  interface{}
+	ErrCode interface{}
+	ErrMsg  interface{}
 }
 
 func (e Err) Error() string {
-    return fmt.Sprintf("ErrCode: %v; ErrMsg: %v", e.ErrCode, e.ErrMsg)
+	return fmt.Sprintf("ErrCode: %v; ErrMsg: %v", e.ErrCode, e.ErrMsg)
 }
