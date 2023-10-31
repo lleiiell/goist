87aa5d0886ee4fd6584c0f1b094c4365ea8c6340
lleiiell <lleiiell@live.com>
2023-10-31 16:16:42 +0800

    file add CsvWrite

--- file.go
+++ file.go
@@ -1,6 +1,9 @@
 package goist
 
-import "os"
+import (
+	"encoding/csv"
+	"os"
+)
 
 func FileExists(filename string) (y bool, err error) {
 	_, errStat := os.Stat(filename)
@@ -14,4 +17,36 @@ func FileExists(filename string) (y bool, err error) {
 
 	y = true
 	return
+}
+
+func FileCreate(filename string) (*os.File, error) {
+	flag := os.O_RDWR | os.O_CREATE | os.O_TRUNC
+	return os.OpenFile(filename, flag, 0640)
+}
+
+func CsvWrite(filename string, records [][]string) (err error) {
+	exists, errE := FileExists(filename)
+	if errE != nil {
+		return errE
+	}
+
+	var f *os.File
+	if !exists {
+		f, err = FileCreate(filename)
+		if err != nil {
+			return err
+		}
+	}
+
+	w := csv.NewWriter(f)
+	err = w.WriteAll(records)
+	if err != nil {
+		return
+	}
+	w.Flush()
+	if err = w.Error(); err != nil {
+		return err
+	}
+
+	return nil
 }
