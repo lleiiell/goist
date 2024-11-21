6c8d03b5cbeca9dcf641209e696ea8c99a537783
lleiiell <lleiiell@live.com>
2024-11-21 10:17:40 +0800

    add example ExampleFloatDivide

--- math_test.go
+++ math_test.go
@@ -20,3 +20,15 @@ func ExampleIdUnmask() {
 	// 999
 	// 6521156348675224222
 }
+
+func ExampleFloatDivide() {
+	a := uint(10)
+	b := uint(3)
+
+	fmt.Println(float64(a / b))
+	fmt.Println(float32(a) / float32(b))
+	fmt.Println(float64(a) / float64(b))
+	// Output: 3
+	// 3.3333333
+	// 3.3333333333333335
+}
