55b90525b5e721c2c9dec40b22e2a175e553d0d1
lleiiell <lleiiell@live.com>
2024-12-17 10:17:46 +0800

    examples add closureEscape, stackEscape, interfaceEscape

--- examples/escape/main.go
+++ examples/escape/main.go
@@ -1,13 +1,9 @@
 package main
 
+import "math/rand"
+
 /*
 go run -gcflags '-m -l' main.go
-# command-line-arguments
-./main.go:29:16: leaking param: name
-./main.go:29:38: leaking param: nick
-./main.go:32:9: &Person{...} escapes to heap
-Alice 10
-
 */
 func main() {
 
@@ -16,8 +12,19 @@ func main() {
 }
 
 func printlnPerson() {
-	person := NewPerson("Alice", 10, "Ali")
+	person := pointEscapePerson("Alice", 10, "Ali")
 	println(person.name, person.age)
+
+	interfaceEscape(person.name)
+
+	stackEscapeGenerate8191()
+
+	stackEscapeGenerate8192()
+
+	stackEscapeGenerate(1)
+
+	incr := closureEscape()
+	incr()
 }
 
 type Person struct {
@@ -26,8 +33,57 @@ type Person struct {
 	age  int
 }
 
-func NewPerson(name string, age int, nick string) *Person {
-	// p := Person{name: name} // 局部变量 p 在函数返回后逃逸
-	// return &p               // 返回指向局部变量的指针
+/*
+leaking param: name
+leaking param: nick
+&Person{...} escapes to heap
+*/
+func pointEscapePerson(name string, age int, nick string) *Person {
 	return &Person{name: name, age: age, nick: nick}
+}
+
+/*
+leaking param content: name
+new(Person) does not escape
+*/
+func interfaceEscape(name interface{}) {
+	var userInfo = new(Person)
+	userInfo.name = name.(string)
+	return
+}
+
+// make([]int, 8192) does not escape
+func stackEscapeGenerate8191() {
+	nums := make([]int, 8192) // = 64KB
+	for i := 0; i < 8191; i++ {
+		nums[i] = rand.Int()
+	}
+}
+
+// make([]int, 8193) escapes to heap
+func stackEscapeGenerate8192() {
+	nums := make([]int, 8193) // > 64KB
+	for i := 0; i < 8192; i++ {
+		nums[i] = rand.Int()
+	}
+}
+
+// make([]int, n) escapes to heap
+func stackEscapeGenerate(n int) {
+	nums := make([]int, n) // 不确定大小
+	for i := 0; i < n; i++ {
+		nums[i] = rand.Int()
+	}
+}
+
+/*
+moved to heap: n
+func literal escapes to heap
+*/
+func closureEscape() func() int {
+	n := 0
+	return func() int {
+		n++
+		return n
+	}
 }
